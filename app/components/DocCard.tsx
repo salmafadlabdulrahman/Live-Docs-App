4d919d61ea4214cf065a4652677e6353dd2270d2
Salma <salmafadlabdulrahman@gmail.com>
2025-01-29 00:24:54 +0200

    update dimension of trash icon

--- app/components/DocCard.tsx
+++ app/components/DocCard.tsx
@@ -15,14 +15,14 @@ const DocCard = () => {
             className="w-full object-cover sm:p-2"
           />
         </div>
-        <div>
+        <div className="">
           <p className="capitalize font-semibold text-[18px] tracking-wide">
             JSM new project requirement
           </p>
           <p className="text-blue-100 mt-[.5em] ">Created about 03 hours ago</p>
         </div>
       </div>
-      <div className="mt-[1.5em] w-[30px] max-w-[50px] sm:w-[40px] cursor-pointer">
+      <div className="mt-[1.5em] w-[35px] max-w-[50px] sm:w-[40px] cursor-pointer pr-1">
         <Image
           src={"/assets/icons/delete.svg"}
           alt="delete icon"
